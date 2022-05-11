1a598bf771b93a73139004f2528548aabb55bda7
André Gomes Pacheco <andregp86@gmail.com>
2022-05-11 12:02:51 -0300

    Add event listeners (w/ bug)

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-// the user shall input the amount of piles he/she wants on the grid (from 16x16 until 100x100)
-let gridSide = 3;
-
-// With this input value, calculate width/input to figure out the width of each tile of the grid
-let tileWidth = (800 / gridSide);
-
-// update the value of the width of each div
-
-
-// capture the main container in javascript
 const container = document.querySelector('#container');
 
-// create input ** 2 divs and append them in the main container
+// user shall input the amount of piles he/she wants on the grid (from 16x16 until 100x100)
+let gridSide = 3;
 let gridTiles = gridSide ** 2;
 
+// creation of divs based on user input
 for (let i = 0; i < gridTiles; i++) {
     let tile = document.createElement('div');
     tile.classList.add('test');
-    // NOT WORKING document.getElementsByClassName('.test') = tileWidth;
     container.appendChild(tile);
 }
+
+// let allTiles = document.getElementsByClassName('test');
+let allTiles = document.querySelectorAll('.test');
+for (tile of allTiles) {
+    tile.addEventListener('mouseover', () => {
+        tile.classList.add("painted");
+    });
+}
+
+// update the value of the width of each div
+let tileWidth = (800 / gridSide) + " px";
+
 
 // create a function that paint a div by clicking on it OR by hovering over it. Once its hovered, the function effect is still valid.
 // the function will either: 1. change bg-color of the div; 2. add a class to the div, and this class shall have a different color
 
+
 // create a clear board function that resets all bg-color or remove the new class of all divs
+function clearBoard() {
+    for (tile of allTiles) {
+        tile.classList.remove("painted");
+    }
+}
 
 // add color options to paint brush
+
