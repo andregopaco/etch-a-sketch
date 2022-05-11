938db42357327cbbae8dc41e9ad023c2eb7a8ebb
André Gomes Pacheco <andregp86@gmail.com>
2022-05-11 00:40:46 -0300

    Write pseudocode on script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// the user shall input the amount of piles he/she wants on the grid (from 16x16 until 100x100)
+
+// With this input value, calculate width/input to figure out the width of each tile of the grid
+
+// update the value of the width of each div
+
+// create a function that paint a div by clicking on it OR by hovering over it. Once its hovered, the function effect is still valid.
+// the function will either: 1. change bg-color of the div; 2. add a class to the div, and this class shall have a different color
+
+// create a clear board function that resets all bg-color or remove the new class of all divs
+
+// add color options to paint brush
