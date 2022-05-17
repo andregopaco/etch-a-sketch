173e6d007921f5820accf24b96780dde022733c6
André Gomes Pacheco <andregp86@gmail.com>
2022-05-17 14:18:17 -0300

    Create changeSize e paintGrid functions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('#container');
-let gridSide = document.querySelector("#inputValue").value;
+let gridSide = 16;
 let gridTiles = gridSide ** 2;
 let tileSize = (800 / gridSide) - 2;
 
@@ -14,21 +14,17 @@ function createGrid(amount) {
 
 createGrid(gridTiles);
 
-// const inputBtn = document.getElementById("inputValue");
-// const val = document.querySelector('#inputValue').value;
+function paintGrid() {
+    let allTiles = document.querySelectorAll('.test');
 
-// inputBtn.addEventListener('input', );
-
-
-let allTiles = document.querySelectorAll('.test');
-
-allTiles.forEach((tile) => { 
-    tile.addEventListener('mouseover', () => {
-        tile.classList.add("painted");
+    allTiles.forEach((tile) => { 
+        tile.addEventListener('mouseover', () => {
+            tile.classList.add("painted");
+        });
     });
-});
+}
 
-
+paintGrid();
 
 function clearBoard() {
     for (tile of allTiles) {
@@ -38,5 +34,40 @@ function clearBoard() {
 
 const clearButton = document.getElementById("clearBtn");
 clearButton.onclick = clearBoard;
-// add color options to paint brush
 
+function clearContainer () {
+    container.innerHTML = "";
+}
+
+function changeSize() {
+    let newSize = prompt("To change the grid size, type a number between 10 and 100.");
+    if (newSize <= 100 && newSize >= 10) {
+        clearContainer();
+        gridSize = newSize;
+        gridTiles = newSize ** 2;
+        tileSize = (800 / newSize) - 2;
+        createGrid(gridTiles);
+        paintGrid();
+    } else {
+        alert("Invalid entry.");
+    }  
+}
+
+const sizeButton = document.getElementById("sizeBtn");
+sizeButton.onclick = changeSize;
+
+
+// const inputBtn = document.getElementById("inputValue");
+// const val = document.querySelector('#inputValue').value;
+
+// inputBtn.addEventListener('input', );
+
+// NOT WORKING - COLORING FUNCTION:
+
+// const tileColor = document.querySelector('.painted');
+// const colorPicker = document.getElementById('cor');
+// colorPicker.addEventListener('click', function (e) {
+//     tileColor = document.getElementById('cor').value;
+// })
+
+// *BUGGY* - tileColor.style.backgroundColor = colorPicker;
